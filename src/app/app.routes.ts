6334547448735957d7d445b3b7675f89dffa6b5e
pujasingla <pujasingla089@gmail.com>
2024-12-10 18:40:05 -0500

    dashboard and upcoming bookings component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,16 @@ import {EventDetailsComponent} from "./components/event-details/event-details.co
 import {AddEventComponent} from "./components/add-event/add-event.component";
 import {LoginComponent} from "./components/login/login.component";
 import {SignupComponent} from "./components/signup/signup.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {UpcomingBookingsComponent} from "./components/upcoming-bookings/upcoming-bookings.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'events', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'events', component: EventListComponent },
+  {path: 'upcomingBookings', component: UpcomingBookingsComponent },
   { path: 'events/:id', component: EventDetailsComponent},
   { path: 'add', component: AddEventComponent }
 
---
+++ src/app/components/upcoming-bookings/upcoming-bookings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upcoming-bookings',
+  standalone: true,
+  templateUrl: './upcoming-bookings.component.html',
+  styleUrl: './upcoming-bookings.component.css'
+})
+export class UpcomingBookingsComponent {
+
+}
